05e995223d3442d4331d847568032bb36f2df476
Broven-l <78793289+Broven-l@users.noreply.github.com>
2021-05-05 21:37:49 +0800

    Add files via upload
    A new version of conway's game of life

--- board.h
+++ board.h
@@ -1,1 +1,17 @@
 #pragma once
+#ifndef BOARD_H
+#define BOARD_H
+
+typedef struct board {
+    char* box;
+    int width;
+    int height;
+} board;
+
+board* createBoard(int w, int h);
+
+int checkNeighbors(board* b, int x, int y);
+
+void generation(board* current, board* next);
+
+#endif
